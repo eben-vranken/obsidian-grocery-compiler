4c6cbedd1a81f439eaf8e009a6331d2179d13437
eben-vranken <vranken.eben@outlook.com>
2024-08-15 20:02:27 +0000

    Added basic menu foundation

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+groceries = []
+
 def main():
     print_menu()
     user_input = input("Enter your choice: ")
-    print(user_input)
+    while user_input != "0":
+	options = {
+"1": view_groceries,
+"2": add_groceries
+	}
+        options.get(user_input,default)()
+            
 
 def print_menu():
     print("1. View groceries")
     print("2. Add groceries")
     print("3. Confirm groceries")
-    print("0. Exit")
+    print("0. Quit")
+
+def view_groceries():
+    print(groceries)
+
+def add_groceries():
+    print("Add groceries")
 
 if __name__ == "__main__":
     main()
